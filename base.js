4cd1e2fd8791588b7c497bd08ec71cafcd302893
Ivy Benson <irosebenson@gmail.com>
2023-02-05 19:05:39 -0600

    hello world

---
+++ base.js
@@ -0,0 +1,5 @@
+// find some boilerplate and practice!
+
+// going to use this as a general dump for my practice between now and my big interview. I would also love to create a larger java project but I think I need IntelliJ for that
+
+console.log('Hello World');
